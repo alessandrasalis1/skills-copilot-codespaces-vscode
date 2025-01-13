494ae14ad286425502263fcdf50567f148008a9a
alessandrasalis1 <alessandra.salis@outlook.it>
2025-01-13 16:31:02 +0000

    Copilot second commit

---
+++ .github/member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    name: 'skills',
+    title: 'Skills',
+    description: 'List of skills that you have',
+    type: 'array',
+    of: [{ type: 'string' }],
+  };
+}/workspaces/skills-copilot-codespaces-vscode/.github/member.js
